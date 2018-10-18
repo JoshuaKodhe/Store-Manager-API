03fad2b768e4b468248a7b02a987b6f83159fc6a
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-18 07:51:06 +0300

    [Feature #161304324] Add test to test the get all product endpoint

--- app/tests/v1/test_products_endpoints.py
+++ app/tests/v1/test_products_endpoints.py
@@ -9,15 +9,19 @@ class TestProductsEndpoint(unittest.TestCase):
     def setUp(self):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
+        self.product_item = {"name": "Chair",
+                             "description": "The product description here",
+                             "quantity": 12,
+                             "category": "Furniture",
+                             "price": 2000}
 
     def test_post_product_method(self):
-        product_item = {"name": "Chair",
-                        "description": "The product description here",
-                        "quantity": 12,
-                        "category": "Furniture",
-                        "price": 2000}
-
         response = self.client().post(BASE_URL,
-                                      data=json.dumps(product_item),
+                                      data=json.dumps(self.product_item),
                                       content_type='application/json')
         self.assertEqual(response.status_code, 201)
+
+    def test_get_all_products_method(self):
+        get_all_products = self.client().get(BASE_URL)
+        data = json.loads(get_all_products.get_data())
+        self.assertEqual(get_all_products.status_code, 200)
