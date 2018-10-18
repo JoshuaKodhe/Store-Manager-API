898a09d6f484f7067a358cf63fd0a902a6026041
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-18 15:43:07 +0300

    [Feature #161312874] Create a model shell for our sales

---
+++ app/api/v1/models/sales_models.py
@@ -0,0 +1,21 @@
+""" Sale Rcord Models """
+
+
+class SaleRecordModel:
+    """ Class the defines how our Sale records will look """
+    sales_list = []
+
+    def __init__(self, sold_by, unit_price, quantity, category):
+        pass
+
+    def save_record(self):
+        """Method to save a sale record"""
+        pass
+
+    def retrieve_records(self):
+        """ Method to get all  sale records"""
+        pass
+
+    def retrieve_single_records(self, saleId):
+        """ Method to get one sale record by the sale_record_id"""
+        pass
