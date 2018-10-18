0f5c4ac39c5a0e9d0caca8895bd81ff5142b7502
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-18 08:01:05 +0300

    [Finishes #161304324] Add our api endpoint for get all products

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 
-from app.api.v1.resources.products_endpoints import ProductEndpoint
+from app.api.v1.resources.products_endpoints import (ProductEndpoint,
+                                                     ProductListEndpoint)
 
 VERSION_1 = Blueprint('API', __name__, url_prefix="/api/v1")
 API = Api(VERSION_1)
 
+API.add_resource(ProductListEndpoint, '/products')
 API.add_resource(ProductEndpoint, '/products')
