1eea85df2179f88090268b15adcf046f12b16db4
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-18 15:47:04 +0300

    [Feature #161312874] Update class sales record with initialization method

--- app/api/v1/models/sales_models.py
+++ app/api/v1/models/sales_models.py
@@ -6,7 +6,11 @@ class SaleRecordModel:
     sales_list = []
 
     def __init__(self, sold_by, unit_price, quantity, category):
-        pass
+        self.sale_id = len(self.sales_list)+1
+        self.sale_attendant = sold_by
+        self.unit_price = unit_price
+        self.product_quantity = quantity
+        self.category = category
 
     def save_record(self):
         """Method to save a sale record"""
