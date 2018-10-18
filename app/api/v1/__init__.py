8ba5460a3f928652524f4c67a3395141709dc4cd
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-18 03:32:30 +0300

    [Feature #161295872] Register the post products endpoint and hide .pyc and cache files

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,5 +3,9 @@ from flask import Blueprint
 from flask_restful import Api
 
 
+from app.api.v1.resources.products_endpoints import ProductEndpoint
+
 VERSION_1 = Blueprint('API', __name__, url_prefix="/api/v1")
 API = Api(VERSION_1)
+
+API.add_resource(ProductEndpoint, '/products')
