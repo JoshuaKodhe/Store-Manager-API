67a0df5df03a34486a92d07cd1630edf013b1776
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-19 07:39:19 +0300

    [Feature #161335232] Create a template of our sales classes endpoints

---
+++ app/api/v1/resources/sales_endpoint.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+from flask import request
+
+
+class SalesRecordEndpoint(Resource):
+    def post(self):
+        pass
+
+    def get(self):
+        pass
+
+
+class SalesRecordsListEndpoint(Resource):
+    def get(self):
+        pass
