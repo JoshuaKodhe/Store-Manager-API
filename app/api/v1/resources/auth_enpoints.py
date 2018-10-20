e0013560ce7a09270ceede7fd62b5b1eae865e2a
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-20 14:27:46 +0300

    [Feature #161360455] Add an access token

--- app/api/v1/resources/auth_enpoints.py
+++ app/api/v1/resources/auth_enpoints.py
@@ -1,10 +1,7 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token
 
 from app.api.v1.models.user_model import User
-parser = reqparse.RequestParser()
-parser.add_argument('username', help='Enter username', required=True)
-parser.add_argument('password', help='Enter email', required=True)
-
 
 parser = reqparse.RequestParser()
 parser.add_argument('email', help='Enter email', required=True)
@@ -18,5 +15,6 @@ class UserRegistrationEndpoint(Resource):
                         password=User.generate_hash(data['password'])
                         )
         new_user.save_user()
+        access_token = create_access_token(identity=data["email"])
         return {"message": f'User {data["email"]} was created',
-                }, 201
+                "access_token": access_token}, 201
