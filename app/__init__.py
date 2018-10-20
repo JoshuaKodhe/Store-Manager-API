0219ccf953e220bd685c290a5f8ec883c355f4b2
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-20 14:25:05 +0300

    [Feature #161360455] Register the authentication token

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 """ Create our application """
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from instance.config import APP_CONFIG
+
+JWT = JWTManager()
 
 
 def create_app(config_name="development"):
@@ -12,4 +15,5 @@ def create_app(config_name="development"):
     from app.api.v1 import VERSION_1 as v1
     app.register_blueprint(v1)
 
+    JWT.init_app(app)
     return app
