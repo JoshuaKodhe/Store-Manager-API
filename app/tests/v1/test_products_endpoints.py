4107514beb41e37b50651f83170fde7f7820e04e
Joshua Kodhe <jkodhe32@gmail.com>
2018-10-20 21:04:33 +0300

    [Finishes #161361720] Add JWT authentication to all the routes

--- app/tests/v1/test_products_endpoints.py
+++ app/tests/v1/test_products_endpoints.py
@@ -4,6 +4,7 @@ from app import create_app
 
 BASE_URL = '/api/v1/products'
 SINGLE_PROD_URL = '/api/v1/products/{}'
+LOGIN_URL = '/api/v1/login'
 
 
 class TestProductsEndpoint(unittest.TestCase):
@@ -16,23 +17,42 @@ class TestProductsEndpoint(unittest.TestCase):
                              "category": "Furniture",
                              "price": 2000}
 
+        self.login_user = {"email": "testuser@gmail.com",
+                           "password": "asdfg",
+                           }
+
+    def login(self):
+        response = self.client().post(LOGIN_URL,
+                                      data=json.dumps(self.login_user),
+                                      content_type='application/json')
+        access_token = json.loads(response.get_data())["access_token"]
+        return access_token
+
     def test_post_product_method(self):
         """ Test for posting a single product """
         response = self.client().post(BASE_URL,
                                       data=json.dumps(self.product_item),
+                                      headers=dict(Authorization="Bearer "+self.login()),
                                       content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_get_all_products_method(self):
         """ Test for getting all products """
-        get_all_products = self.client().get(BASE_URL)
+        get_all_products = self.client().get(BASE_URL,
+                                             headers=dict(Authorization="Bearer "+self.login()),
+                                             content_type='application/json')
         self.assertEqual(get_all_products.status_code, 200)
 
     def test_get_single_product(self):
         """ Test for getting a single product """
         response = self.client().post(BASE_URL,
                                       data=json.dumps(self.product_item),
+                                      headers=dict(Authorization="Bearer "+self.login()),
                                       content_type='application/json')
+
         data = json.loads(response.get_data())
-        single_product = self.client().get(SINGLE_PROD_URL.format(data['Product']['product_id']))
+        print(data)
+        single_product = self.client().get(SINGLE_PROD_URL.format(data['Product']['product_id']),
+                                           headers=dict(Authorization="Bearer "+self.login()),
+                                           content_type='application/json')
         self.assertEqual(single_product.status_code, 200)
