4a07008314da6b94f8bec4cbe5b8042d492f0a71
Joshua Kodhe <jkodhe32@gmail.com>
2018-11-08 13:28:46 +0300

    [Chore #161404659] Updated logic

--- app/api/v1/resources/auth_enpoints.py
+++ app/api/v1/resources/auth_enpoints.py
@@ -21,22 +21,23 @@ class UserRegistrationEndpoint(Resource):
             if item not in payload:
                 return {"message": f"The field {item} is not a valid field"}, 400
 
-        if not User.fetch_single_user(email):
-            if not email:
-                return{"message": "Please enter a valid email"}, 400
-            elif not username:
-                return {"message": "Please enter a username"}, 400
-            elif not password:
-                return {"message": "please enter password"}, 400
-            else:
+
+        if not email:
+            return{"message": "Please enter a valid email"}, 400
+        elif not username:
+            return {"message": "Please enter a username"}, 400
+        elif not password:
+            return {"message": "please enter password"}, 400
+        else:
+            if not User.fetch_single_user(email):
                 new_user = User(email=data['email'],
                                 password=User.generate_hash(data['password'])
                                 )
                 new_user.save_user()
                 return {"message": f'User {data["email"]} was created'}, 201
-            return {"message": 'Please enter an email address and password',
-                    }, 400
-        return {"message": f"User {email} already exists"}, 400
+            return {"message": f"User {email} already exists"}, 400
+        return {"message": 'Please enter an email address and password',
+                }, 400
 
 
 class UserLoginEndpoint(Resource):
